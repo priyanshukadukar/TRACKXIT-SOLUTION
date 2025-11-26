2b17773b5a8a9e1e0af9743fe17bc6a8f588b395
Priyanshu Kadukar <priyanshukadukar26@gmail.com>
2025-11-26 13:08:50 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,275 +32,306 @@
 ======================================*/
 
 (function ($) {
- "use strict";
+  "use strict";
 
-    jQuery(document).ready(function($){
-		
-		/* ==== 01. jQuery MeanMenu Active ==== */
-		if ($.fn.meanmenu) {
-			jQuery('.bizes-nav').meanmenu();	
-		}
-		
-		/* ==== 02. Drone Testimonial ==== */
-		 if ($.fn.owlCarousel) {
-		 	$('.drone-testimonial-wrap').owlCarousel({
-		 		autoplay:false,
-		 		items:1,
-		 		loop:true,
-		 		autoplayHoverPause: false,
-				smartSpeed: 500,
-		 		margin:0,
-				nav:true,
-		 		navText:['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
-			dots:true,
-		 	}) 
+  jQuery(document).ready(function ($) {
+    /* ==== 01. jQuery MeanMenu Active ==== */
+    if ($.fn.meanmenu) {
+      jQuery(".bizes-nav").meanmenu();
+    }
 
-		 	/* ==== 03. Bike Testimonial ==== */
-			$('.bike-testimonial-carousle').owlCarousel({
-				autoplay:false,
-				items:1,
-				loop:true,
-				autoplayHoverPause: false,
-				smartSpeed: 500,
-				margin:0,
-				nav:true,
-				navText:['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
-				dots:true,
-			}) 
+    /* ==== 02. Drone Testimonial ==== */
+    if ($.fn.owlCarousel) {
+      $(".drone-testimonial-wrap").owlCarousel({
+        autoplay: false,
+        items: 1,
+        loop: true,
+        autoplayHoverPause: false,
+        smartSpeed: 500,
+        margin: 0,
+        nav: true,
+        navText: [
+          '<i class="fa fa-long-arrow-left"></i>',
+          '<i class="fa fa-long-arrow-right"></i>',
+        ],
+        dots: true,
+      });
 
-			/* ==== 04. Headphone Testimonial Carousel ==== */
-			
-				$('.music-testimonial-wrap').owlCarousel({
-					autoplay:false,
-					items:2,
-					loop:true,
-					autoplayHoverPause: false,
-					smartSpeed: 500,
-					margin:30,
-					nav:true,
-					navText:['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
-					dots:true,
-					responsiveClass:true,
-					responsive:{
-						0:{
-							items:1,
-						},
-						600:{
-							items:2,
-						},
-						768:{
-							items:2,
-						},
-						1000:{
-							items:2,
-						}
-					}
-				}) 
-		/* ==== 05. Headphone Gallery area ==== */
-			$('.music-gallery-wrap').owlCarousel({
-				autoplay:false,
-				items:4,
-				loop:true,
-				autoplayHoverPause: false,
-				smartSpeed: 500,
-				margin:30,
-				center:true,
-				nav:false,
-				navText:['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
-				dots:false,
-				responsiveClass:true,
-				responsive:{
-					0:{
-						items:1,
-					},
-					600:{
-						items:2,
-					},
-					768:{
-						items:3,
-					},
-					1000:{
-						items:4,
-					}
-				}
-			})
+      /* ==== 03. Bike Testimonial ==== */
+      $(".bike-testimonial-carousle").owlCarousel({
+        autoplay: false,
+        items: 1,
+        loop: true,
+        autoplayHoverPause: false,
+        smartSpeed: 500,
+        margin: 0,
+        nav: true,
+        navText: [
+          '<i class="fa fa-long-arrow-left"></i>',
+          '<i class="fa fa-long-arrow-right"></i>',
+        ],
+        dots: true,
+      });
 
-		/* ==== 06. Live cam Testimonial Carousel ==== */
-			$('.cam-testimonial-wrap').owlCarousel({
-				autoplay:false,
-				items:3,
-				loop:true,
-				autoplayHoverPause: false,
-				smartSpeed: 500,
-				margin:30,
-				nav:false,
-				navText:['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
-				dots:false,
-				responsiveClass:true,
-				responsive:{
-					0:{
-						items:1,
-					},
-					600:{
-						items:2,
-					},
-					768:{
-						items:2,
-					},
-					1000:{
-						items:3,
-					}
-				}
-			}) 
+      /* ==== 04. Headphone Testimonial Carousel ==== */
 
-			/* ==== 07. App Testimonial Carousel ==== */
-			$('.apptestimonial-wrap').owlCarousel({
-				autoplay:false,
-				items:1,
-				loop:true,
-				autoplayHoverPause: false,
-				smartSpeed: 500,
-				margin:30,
-				nav:true,
-				navText:['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
-				dots:false,
-			}) 
+      $(".music-testimonial-wrap").owlCarousel({
+        autoplay: false,
+        items: 2,
+        loop: true,
+        autoplayHoverPause: false,
+        smartSpeed: 500,
+        margin: 30,
+        nav: true,
+        navText: [
+          '<i class="fa fa-long-arrow-left"></i>',
+          '<i class="fa fa-long-arrow-right"></i>',
+        ],
+        dots: true,
+        responsiveClass: true,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 2,
+          },
+          768: {
+            items: 2,
+          },
+          1000: {
+            items: 2,
+          },
+        },
+      });
+      /* ==== 05. Headphone Gallery area ==== */
+      $(".music-gallery-wrap").owlCarousel({
+        autoplay: false,
+        items: 4,
+        loop: true,
+        autoplayHoverPause: false,
+        smartSpeed: 500,
+        margin: 30,
+        center: true,
+        nav: false,
+        navText: [
+          '<i class="fa fa-long-arrow-left"></i>',
+          '<i class="fa fa-long-arrow-right"></i>',
+        ],
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 2,
+          },
+          768: {
+            items: 3,
+          },
+          1000: {
+            items: 4,
+          },
+        },
+      });
 
+      /* ==== 06. Live cam Testimonial Carousel ==== */
+      $(".cam-testimonial-wrap").owlCarousel({
+        autoplay: false,
+        items: 3,
+        loop: true,
+        autoplayHoverPause: false,
+        smartSpeed: 500,
+        margin: 30,
+        nav: false,
+        navText: [
+          '<i class="fa fa-long-arrow-left"></i>',
+          '<i class="fa fa-long-arrow-right"></i>',
+        ],
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 2,
+          },
+          768: {
+            items: 2,
+          },
+          1000: {
+            items: 3,
+          },
+        },
+      });
 
-			/* ==== 08. App Logo Carousel ==== */
-			$('.app-logocarousel-wrap').owlCarousel({
-				autoplay:false,
-				items:4,
-				loop:true,
-				autoplayHoverPause: false,
-				smartSpeed: 500,
-				margin:30,
-				nav:false,
-				navText:['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
-				dots:false,
-				responsiveClass:true,
-				responsive:{
-					0:{
-						items:1,
-					},
-					600:{
-						items:2,
-					},
-					768:{
-						items:3,
-					},
-					1000:{
-						items:4,
-					}
-				}
-			}) 
+      /* ==== 07. App Testimonial Carousel ==== */
+      $(".apptestimonial-wrap").owlCarousel({
+        autoplay: false,
+        items: 1,
+        loop: true,
+        autoplayHoverPause: false,
+        smartSpeed: 500,
+        margin: 30,
+        nav: true,
+        navText: [
+          '<i class="fa fa-long-arrow-left"></i>',
+          '<i class="fa fa-long-arrow-right"></i>',
+        ],
+        dots: false,
+      });
 
-			/* ==== 09. Watch hero ==== */
-			$('.watch-img-carousel-wrap').owlCarousel({
-				autoplay:true,
-				items:1,
-				loop:true,
-				autoplayHoverPause: false,
-				smartSpeed: 500,
-				margin:0,
-				nav:false,
-				dots:false,
-			}) 
+      /* ==== 08. App Logo Carousel ==== */
+      $(".app-logocarousel-wrap").owlCarousel({
+        autoplay: false,
+        items: 4,
+        loop: true,
+        autoplayHoverPause: false,
+        smartSpeed: 500,
+        margin: 30,
+        nav: false,
+        navText: [
+          '<i class="fa fa-long-arrow-left"></i>',
+          '<i class="fa fa-long-arrow-right"></i>',
+        ],
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 2,
+          },
+          768: {
+            items: 3,
+          },
+          1000: {
+            items: 4,
+          },
+        },
+      });
 
-			/* ==== 10. App Screenshot ==== */
-			$('.screenshot-wrap').owlCarousel({
-				autoplay:false,
-				items:4,
-				loop:true,
-				autoplayHoverPause: false,
-				smartSpeed: 500,
-				autoWidth: false,
-				margin:5,
-				center:true,
-				nav:true,
-				navText:['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
-				dots:false,
-				responsiveClass:true,
-				responsive:{
-					0:{
-						items:1,
-					},
-					600:{
-						items:2,
-					},
-					768:{
-						items:3,
-					},
-					1000:{
-						items:4,
-					}
-				}
-			}) 
+      /* ==== 09. Watch hero ==== */
+      $(".watch-img-carousel-wrap").owlCarousel({
+        autoplay: true,
+        items: 1,
+        loop: true,
+        autoplayHoverPause: false,
+        smartSpeed: 500,
+        margin: 0,
+        nav: false,
+        dots: false,
+      });
 
-		 }
+      /* ==== 10. App Screenshot ==== */
+      $(".screenshot-wrap").owlCarousel({
+        autoplay: false,
+        items: 4,
+        loop: true,
+        autoplayHoverPause: false,
+        smartSpeed: 500,
+        autoWidth: false,
+        margin: 5,
+        center: true,
+        nav: true,
+        navText: [
+          '<i class="fa fa-long-arrow-left"></i>',
+          '<i class="fa fa-long-arrow-right"></i>',
+        ],
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 2,
+          },
+          768: {
+            items: 3,
+          },
+          1000: {
+            items: 4,
+          },
+        },
+      });
+    }
 
-    	/*====  11. one page js active =====*/
-        $('.main-menu-wrap > .bizes-nav > ul.menu > li > a').on("click", function() {
-            //Toggle Class
-            $(".active").removeClass("active");
-            $(this).closest('li').addClass("active");
-            var theClass = $(this).attr("class");
-            $('.' + theClass).parent('li').addClass('active');
-            //Animate
-            $('html, body').stop().animate({
-                scrollTop: $($(this).attr('href')).offset().top - 100
-            }, 1000);
-            return false;
-        });
+    /*====  11. one page js active =====*/
+    $(".main-menu-wrap > .bizes-nav > ul.menu > li > a").on(
+      "click",
+      function () {
+        //Toggle Class
+        $(".active").removeClass("active");
+        $(this).closest("li").addClass("active");
+        var theClass = $(this).attr("class");
+        $("." + theClass)
+          .parent("li")
+          .addClass("active");
+        //Animate
+        $("html, body")
+          .stop()
+          .animate(
+            {
+              scrollTop: $($(this).attr("href")).offset().top - 100,
+            },
+            1000
+          );
+        return false;
+      }
+    );
 
+    /* ==== 12. Venobox Active ==== */
+    $(".venobox").venobox();
 
-		/* ==== 12. Venobox Active ==== */	
-			$('.venobox').venobox(); 
-
-		/* ==== 11. ScrollUp ==== */
-		$.scrollUp({
-			scrollText: '<i class="fa fa-arrow-up"></i>',
-			easingType: 'linear',
-			scrollSpeed: 900,
-			animation: 'fade'
-		}); 
-	
+    /* ==== 11. ScrollUp ==== */
+    $.scrollUp({
+      scrollText: '<i class="fa fa-arrow-up"></i>',
+      easingType: "linear",
+      scrollSpeed: 900,
+      animation: "fade",
     });
-	
-	/* ==== 13. Sticky Header ==== */
-	$(window).on('scroll',function() {
-		var header_ = $(".header-area.primary-header");
+  });
 
-	  if ($(this).scrollTop() > 1){  
-		header_.addClass("scroll-header");
-	    }
-	  else{
-		header_.removeClass("scroll-header");
-	    }
-	}); 
-	
-	/* ==== 14. Preloader ==== */
-	$(window).on('load',function(){
-		jQuery(".preloader-wrap").fadeOut(500);
-		/* ==== 15. Wow active ==== */
-		new WOW().init();
-	});
-	
-	
-})(jQuery); 
+  /* ==== 13. Sticky Header ==== */
+  $(window).on("scroll", function () {
+    var header_ = $(".header-area.primary-header");
+
+    if ($(this).scrollTop() > 1) {
+      header_.addClass("scroll-header");
+    } else {
+      header_.removeClass("scroll-header");
+    }
+  });
+
+  /* ==== 14. Preloader ==== */
+  $(window).on("load", function () {
+    jQuery(".preloader-wrap").fadeOut(500);
+    /* ==== 15. Wow active ==== */
+    new WOW().init();
+  });
+})(jQuery);
 
 // Testimonial Widget Code
 document.addEventListener("DOMContentLoaded", () => {
   const cards = document.querySelectorAll(".testimonial-card");
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("fade-in");
-        observer.unobserve(entry.target);
-      }
-    });
-  }, { threshold: 0.2 });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("fade-in");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
 
   cards.forEach((card) => observer.observe(card));
 });
+
+// Industry Section Edit
